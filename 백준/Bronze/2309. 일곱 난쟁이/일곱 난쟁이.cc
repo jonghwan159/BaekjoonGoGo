98e5f7121ecfe448b32af44e64678b9c7b67d3be
choi jonghwan <145255067+jonghwan159@users.noreply.github.com>
2025-05-06 17:08:46 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,66 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n = 9, k = 7;
-int key = 0;
-vector<int> v;
-void print(vector<int>b)
-{
-    for(int i : b) cout << i << " ";
-    cout << "\n";
-}
 
-void combi (int start, vector<int> &b)
-{
-    if(key == 1)
-    {
-        return;
-    }
-    if(b.size() == k)
-    {
-        //logic
-        int sum = 0;
-        
-        for(int i = 0 ; i < k ; i++)
-        {
-            sum += v[b[i]];
-        }
-
-        if(sum == 100)
-        {
-            key = 1;
-            vector<int> answer;
-            for(int i = 0 ; i < k ; i++)
-            {
-                answer.push_back(v[b[i]]);
-            }
-            sort(answer.begin(),answer.end());
-            for(int i = 0 ; i < k ; i++)
-            {
-                cout<< answer[i]<<"\n";
-            }
-        }
-        return;
-    }
-
-    for(int i = start+1 ; i < n ; i++)
-    {
-        b.push_back(i);
-        combi(i, b);
-        b.pop_back();
-    }
-    return;
-}
-
-int main ()
-{
+int main()
+{   
+    int a[9];
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    for(int i = 0 ; i < 9 ; i++){
-        int temp;
-        cin >> temp;
-        v.push_back(temp);
+    for(int  i = 0 ; i <9 ; i++) cin >> a[i];
+    sort(a,a+9);
+    do
+    {
+        int sum = 0;
+        for(int i=0; i< 7; i++) sum+= a[i];
+
+        if(sum == 100) break;
     }
-    vector<int> b;
-    combi(-1,b);
-    return 0 ;
+    while(next_permutation(a,a+9));
+
+    for(int i=0 ; i < 7 ; i++)
+    {   
+        cout<< a[i]<<'\n';
+    }
 }
