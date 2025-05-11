4e48fe431a86e424d03f8d55e2b4a3966ba70b14
최종환 <145255067+jonghwan159@users.noreply.github.com>
2025-05-11 10:23:55 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,33 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int Acnt, Bcnt, Ccnt, A, B ,C, Min = 101, Max = -1;
-int Cnt[301];
-vector<pair<int,int>> v(3);
-int main()
-{
-    cin >> A >> B >> C;
-    for(int i = 0 ; i < 3; i++) cin >> v[i].first >> v[i].second;
-    
-    for(auto t : v){
-       // cout << "t.first = " << t.first <<" t.second =" << t.second << "\n";
-        if (Min > t.first) Min = t.first;
-        if (Max < t.second) Max = t.second;
-        for(int i = t.first ; i < t.second ; i++)
-        {
-            Cnt[i]++;
-        }
-    }
-  
-    //cout<< "Min = " << Min << " Max = " << Max << "\n"; 
-    for(int i = Min; i < Max; i++)
-    {
-        if(Cnt[i] == 1) Acnt++;
-        else if(Cnt[i] == 2) Bcnt++;
-        else if(Cnt[i] == 3) Ccnt++;
-    }
-    int answer = Acnt * A + Bcnt * B *2 + Ccnt * C * 3;
+int A,B,C,Acnt,Bcnt,Ccnt;
+int Min = -1, Max = -1;
+vector<pair<int,int>> timeArr(3);
+int cnt[300];
 
-    cout << answer << "\n";
+int main()
+{   
+    ios :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> A >> B >> C ;
+    for(int i = 0 ; i < 3 ; i++) cin >> timeArr[i].first >> timeArr[i].second;
+
+    for(auto t : timeArr)
+    {
+        if(Min > t.first) Min = t.first;
+        if(Max < t.second) Max = t.second;
+
+        for(int i = t.first -1 ; i < t.second-1; i++) cnt[i]++;
+        
+    }
+
+    for(int i = Min-1;  i < Max-1 ; i++ )
+    {
+        if(cnt[i] == 1) Acnt++;
+        if(cnt[i] == 2) Bcnt++;
+        if(cnt[i] == 3) Ccnt++;
+
+        
+    }
+
+    //calcul
+    int answer = A*Acnt + 2*B*Bcnt + 3*C*Ccnt;
+    cout << answer;
     return 0;
 }
