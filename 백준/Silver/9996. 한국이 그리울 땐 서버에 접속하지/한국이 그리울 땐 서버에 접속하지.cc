4e2323bc7403c07257dde7b1a10d113c36dff409
최종환 <145255067+jonghwan159@users.noreply.github.com>
2025-05-28 21:25:11 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,89 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, trigger;
-vector<char> arr1, arr2;
-vector<string> str_arr;
-string pattern;
+int n;
+string s, ori_s, pre,suf;
 int main()
 {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> N;
-    cin >> pattern;
-    for(int i = 0 ; i < pattern.size(); i++)
+    cin >> n;
+    cin >> ori_s;
+    int pos = ori_s.find('*');
+    pre = ori_s.substr(0,pos);
+    suf = ori_s.substr(pos + 1);
+    for(int i = 0; i< n ; i++)
     {
-        if(pattern[i] == '*')
+        cin >> s;
+        if(pre.size() + suf.size() > s.size())
         {
-            trigger = 1;
-            continue;
+            cout <<"NE\n";
         }
-        if(trigger)
-        {
-          arr2.push_back(pattern[i]);  
-        }
-        else
-        {
-          arr1.push_back(pattern[i]);    
-        }
-    }
-    // for(int i = 0; i< arr1.size();i++)
-    //     {
-    //         cout << arr1[i];
-    //     }
-    // cout <<"\n";
-
-    //  for(int i = 0; i< arr2.size();i++)
-    //     {
-    //         cout << arr2[i];
-    //     }
-    // cout <<"\n";
-
-    int minSize = arr1.size()+arr2.size();
-    for(int i =0 ;i < N; i++)
-    {
-        string tempStr;
-        cin >> tempStr;
-        str_arr.push_back(tempStr);
-    }
-
-    for(auto kStr : str_arr)
-    {   
-        
-        int isOK =1;
-        if(kStr.size() < minSize)
-        {
-            cout << "NE\n";
-            continue;
-        }
-        //검사
-        for(int i = 0 ; i<arr1.size();i++)
-        {
-            if(arr1[i] != kStr[i])
+        else{
+            if(pre == s.substr(0,pre.size()) && suf == s.substr(s.size()- suf.size()))
             {
-                isOK = 0;
-                break;
+                cout <<"DA\n";
+            }
+            else{
+                cout <<"NE\n";
             }
         }
-        if(isOK){
-            int max_index = kStr.size()-1;
-            
-            for(int i = arr2.size()-1 ; i>= 0;i--)
-            {   
-                if(arr2[i] != kStr[max_index--])
-                {
-                    
-                    isOK = 0;
-                    break;
-                }
-            }
-        }
-       if(isOK == 0)
-       {
-           cout << "NE\n"; 
-       }
-       else
-       {
-            cout << "DA\n";      
-       }
     }
-    
+    return 0;
 }
