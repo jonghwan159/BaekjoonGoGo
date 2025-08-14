09f68f7df8c424ffbd7201799be9dca48da1d744
최종환 <145255067+jonghwan159@users.noreply.github.com>
2025-08-14 12:10:39 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,54 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int n,size;
+int n;
 string s;
-char grid[65][65];
-
+char a[65][65];
 string quard(int y, int x, int size)
 {
     if(size == 1)
     {
-        return string(1, grid[y][x]);
+        return string(1, a[y][x]); 
     }
-    char b = grid[y][x];
-     //(y, x)부터 (y+size, x+size)까지 스캔
-     // 4개일때 생각하기
-    for(int i = y ; i < y+size; i++)
+
+    char b = a[y][x];
+    for(int i = y; i < y+size; i++)
     {
-        for(int j = x ; j < x+size ; j++)
+        for(int j = x; j < x+size; j++)
         {
-            if(b !=  grid[i][j])
+            if(b != a[i][j])
             {
                 string ret = "(";
-                ret += quard(y,x,size/2);
-                ret += quard(y,x+size/2,size/2);
-                ret += quard(y+size/2,x,size/2);
-                ret += quard(y+size/2,x+size/2,size/2);
+                int sz =size/2;
+                ret += quard(y,x,sz);
+                ret += quard(y,x+sz,sz);
+                ret += quard(y+sz,x,sz);
+                ret += quard(y+sz,x+sz,sz);
                 ret += ')';
                 return ret;
-
-            }    
-         
+            }
         }
     }
-    return string(1, grid[y][x]);
+    return string(1, a[y][x]);
+
 }
+
 
 int main()
 {
-    ios:: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(false); cin.tie(); cout.tie();
     cin >> n;
-    for(int i = 0 ; i < n ; i++)
+
+    for(int i = 0; i < n; i++)
     {
         cin >> s;
         for(int j = 0 ; j < n ; j++)
         {
-            grid[i][j] = s[j];
+            a[i][j] = s[j];
         }
     }
-
-    cout << quard(0, 0, n);
-  
+    cout << quard(0,0,n);
     return 0;
 }
