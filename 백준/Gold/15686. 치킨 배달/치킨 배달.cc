b1f386263629fd700f0af0bf12ea238ca6cb0927
최종환 <145255067+jonghwan159@users.noreply.github.com>
2025-08-21 15:35:58 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,19 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,m,cnt;
-int a[51][51];
-int visited[51][51],ret = 9999999;
-vector<pair<int,int>> chiken;
-vector<pair<int,int>> home;
-vector<vector<int>> chickList;
+int n,m,a[54][54], result = 9e8;
+vector<pair<int,int>> _home,chiken;
+vector<vector<int>> chickenList;
 
 void combi(int start, vector<int> b)
 {
 
     if(b.size() == m)
     {
-        chickList.push_back(b);
+        chickenList.push_back(b);
+        return;
     }
     for(int i = start + 1; i < chiken.size() ; i++)
     {
@@ -52,7 +50,7 @@ int main()
         {
             cin >> a[i][j];
             if(a[i][j] == 2) chiken.push_back({i,j});
-            if(a[i][j] == 1) home.push_back({i,j});
+            if(a[i][j] == 1) _home.push_back({i,j});
         
         }
     }
@@ -64,37 +62,25 @@ int main()
     combi(-1,b);
     
     
-    // for(int i = 0 ; i< chickList.size(); i++)
-    // {
-    //     for(auto j : chickList[i])
-    //     {
-    //         cout << j <<" ";
-    //     }
-    //     cout <<"\n";
-    // }
-    for(int chick = 0 ; chick< chickList.size(); chick++)
+    for(auto cList : chickenList)
     {
-        vector<pair<int,int>> hi;
-         for(auto j : chickList[chick])
-        {
-            hi.push_back(chiken[j]);
-        }
-      
-        // for(auto h : hi)
-        // {
-        //     cout <<"h.first =" <<h.first<<", h.second ="<<h.second <<"\n";
-        // }
-        // cout <<"\n";
-       
-        
         int sum = 0;
-        for(auto i : home)
+        for(auto home : _home)
         {
-            sum += go(i.first,i.second, hi);
+            int chickDist = 100;
+            for(auto ch : cList)
+            {
+                int chikenY = chiken[ch].first;
+                int chikenX = chiken[ch].second;
+                //집 치킨 거리 계산
+                int dist = abs(home.first - chikenY) + abs(home.second - chikenX);
+                chickDist = min(chickDist,dist);
+            }
+            sum += chickDist;
         }
-        ret = min(ret,sum);
+        result = min(result,sum);
     }
-    cout << ret;
+    cout << result;
 
     return 0;
 }
