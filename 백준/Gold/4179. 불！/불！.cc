400efb2cec4c6b1afbc97b6d3adbf114b4010ea6
최종환 <145255067+jonghwan159@users.noreply.github.com>
2025-08-22 15:22:32 +0900

    [Gold III] Title: 불!, Time: 40 ms, Memory: 11016 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,85 +1,98 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int INF = 987654321;
+int r,c,x,y;
 char a[1004][1004];
-int n,m,sx,sy,y,x,ret;
-int dy[] = {1,0,-1,0};
-int dx[] = {0,1,0,-1};
-//불과 
-int fire_visited[1004][1004], person_visited[1004][1004];
-
-bool check_side(int y, int x)
+int people_visited[1004][1004], fire_visited[1004][1004];
+pair<int,int> start_point;
+vector<pair<int,int>> fire_point;
+queue<pair<int,int>> q;
+int dy[] = {1, 0 , -1 ,0};
+int dx[] = {0, 1 , 0 ,-1};
+void printPeople()
 {
-    if(y ==0 || x == 0 || y == n -1 || x == m-1) return 1;
-
-    return 0;
+    for(int i =0 ;i< r ;i++)
+    {
+        for(int j = 0 ; j< c ; j++)
+        {
+            cout << people_visited[i][j] <<" ";
+        }
+        cout <<"\n";
+    }
+    return;
 }
-
+void printFire()
+{
+     for(int i =0 ;i< r ;i++)
+    {
+        for(int j = 0 ; j< c ; j++)
+        {
+            cout << fire_visited[i][j] <<" ";
+        }
+        cout <<"\n";
+    }
+    return;
+}
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    queue<pair<int,int>> q;
-    cin >> n >> m;
+    cin >> r >> c;
     fill(&fire_visited[0][0],&fire_visited[0][0] + 1004 * 1004, INF);
-    for(int i =0 ; i <n ; i++)
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    for(int i =0 ;i< r ;i++)
     {
-        for(int j =0 ; j < m ; j++)
+        for(int j = 0 ; j< c ; j++)
         {
             cin >> a[i][j];
-            if(a[i][j] == 'F')
+            if(a[i][j] == 'J') start_point = {i,j};
+            else if(a[i][j] == 'F')
             {
-                fire_visited[i][j] = 1;
-                q.push({i,j}); 
-            }
-            else if(a[i][j] == 'J')
-            {
-                sy = i;
-                sx = j;
-            }
+                fire_point.push_back({i,j});
+                q.push({i,j});
+                fire_visited[i][j] = 1; 
+            } 
         }
     }
-
     //불 BFS
     while(q.size())
     {
         tie(y,x) = q.front();
         q.pop();
-        for(int i = 0 ; i < 4; i++)
+        for(int i =0 ; i < 4 ; i++)
         {
-           	int ny = y + dy[i];
-			int nx = x + dx[i];
-            if(ny < 0 || ny >= n || nx < 0 || nx >= m || fire_visited[ny][nx] != INF || a[ny][nx] == '#') continue;
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || ny >=r || nx <0 || nx >=c || fire_visited[ny][nx] != INF || a[ny][nx] == '#') continue;
             fire_visited[ny][nx] = fire_visited[y][x] + 1;
-            q.push({ny,nx}); 
+            q.push({ny,nx});
         }
     }
-    // queue에 아무것도 없음 이제
-    person_visited[sy][sx] = 1;
-    q.push({sy,sx});
+    //printFire();
+    q.push({start_point.first,start_point.second});
+    people_visited[start_point.first][start_point.second] = 1;
+    //사람 BFS;
     while(q.size())
     {
         tie(y,x) = q.front();
         q.pop();
-        if(check_side(y,x))
+        if(y == 0 || y == r-1 || x == 0 || x == c-1)
         {
-            ret = person_visited[y][x];
-            break;
+            cout << people_visited[y][x] <<"\n";
+            //rintPeople();
+            return 0;
         }
-
-        for(int i = 0 ; i < 4; i++)
+        for(int i =0 ; i < 4 ; i++)
         {
-           	int ny = y + dy[i];
-			int nx = x + dx[i];
-            if(ny < 0 || ny >= n || nx < 0 || nx >= m || person_visited[ny][nx] || a[ny][nx] == '#') continue;
-            if(fire_visited[ny][nx] <= person_visited[y][x] + 1) continue;
-            person_visited[ny][nx] = person_visited[y][x] + 1;
-            q.push({ny,nx}); 
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || ny >=r || nx <0 || nx >=c || people_visited[ny][nx] || a[ny][nx] != '.') continue;
+            if(fire_visited[ny][nx] <= people_visited[y][x] + 1) continue;
+            people_visited[ny][nx] = people_visited[y][x] + 1;
+            q.push({ny,nx});
         }
     }
-    if(ret != 0) cout << ret<<"\n";
-    else cout << "IMPOSSIBLE \n";
+    cout << "IMPOSSIBLE" <<"\n";
+ 
+
     return 0;
 }
