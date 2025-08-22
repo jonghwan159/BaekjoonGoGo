00c6d17d41a6c2296cd28a03c3d46408a756f4ef
최종환 <145255067+jonghwan159@users.noreply.github.com>
2025-08-22 18:36:57 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 4500 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAX = 200000;
+int visited[MAX+4];
+long long cnt[MAX+4];
+
+int main()
+{
+
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, m;
+    cin >> n >> m;
+    if(n == m)
+    {
+        puts("0");
+        puts("1");
+        return 0;
+    }
+    visited[n] = 1;
+    cnt[n] = 1;
+    queue<int> q;
+    q.push(n);
+    while(!q.empty())
+    {
+        int cx = q.front();
+        q.pop();
+        // cout << "cx = " << cx << ", cnt = " << cnt[cx] <<"\n";
+        for(int nx :{cx+1,cx-1,cx*2} )
+        {
+            if(0 <= nx && nx <= 100000)
+            {
+                if(!visited[nx])
+                {
+                    q.push(nx);
+                    visited[nx] =  visited[cx]+1;
+                    cnt[nx] += cnt[cx];
+                }
+                //방문했고 값이 최단 거리이면
+                else if(visited[nx] == visited[cx]+1 )
+                {
+                    cnt[nx] += cnt[cx];
+                }
+            }
+        }
+    }
+    cout << visited[m] - 1 << "\n";
+    cout << cnt[m] << " \n";
+    return 0;
+
+}
